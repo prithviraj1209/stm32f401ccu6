bae8c36345fe9160c053037a28b30868dd686944
prithviraj1209 <iprithvirajmohite@gmail.com>
2024-10-27 12:31:17 +0530

    changes to i2c source

--- source/i2c_stm32f401ccu6.c
+++ source/i2c_stm32f401ccu6.c
@@ -1,44 +1,44 @@
 #include "stm32f4xx.h"
 
-void i2c_config(void){
-/************* STEPS FOLLOWED*************************
- * 
- * 1. Enable I2C clock and GPIO clock.
- * 2. Configure I2C pins for Alternate Function.
- *      a. Select Alternate Function in MODER register.
- *      b. Select Open-Drain output.
- *      c. Select HIGH SPEED for pins.
- *      d. Select PULL UP for pins.
- *      e. Configure Alternate Function in AFR register.
- * 3. Reset the I2C.
- * 4. Program the input peripheral clock in I2C_CR2 register.
- * 5. Configure the clock control register.
- * 6. Configure the rise time register.
- * 7. Program the I2C_CR1 register to enable the peripheral.
- * 
- ****************************************************/
+// Define SDA1 and SCL1 pins
+#define SDA1_PIN 9  // Choose either 7 or 9 for SDA1
+#define SCL1_PIN 8  // Choose either 6 or 8 for SCL1
+
+void I2C1_Init(void) {
     // 1. Enable I2C clock and GPIO clock
     RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;  // Enable I2C1 clock
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // Enable GPIOB clock
 
     // 2. Configure I2C pins for Alternate Function
     // a. Select Alternate Function in MODER register
-    GPIOB->MODER &= ~((0x3 << (8 * 2)) | (0x3 << (9 * 2))); // Clear MODER8 and MODER9
-    GPIOB->MODER |= (0x2 << (8 * 2)) | (0x2 << (9 * 2));    // Set Alternate Function (AF) for PB8 (I2C1_SCL) and PB9 (I2C1_SDA)
+    GPIOB->MODER &= ~((0x3 << (SCL1_PIN * 2)) | (0x3 << (SDA1_PIN * 2))); // Clear MODER for SCL1 and SDA1
+    GPIOB->MODER |= (0x2 << (SCL1_PIN * 2)) | (0x2 << (SDA1_PIN * 2));    // Set Alternate Function (AF) for chosen SCL1 and SDA1
 
     // b. Select Open-Drain output
-    GPIOB->OTYPER |= (0x1 << 8) | (0x1 << 9); // Set Open-Drain for PB8 and PB9
+    GPIOB->OTYPER |= (0x1 << SCL1_PIN) | (0x1 << SDA1_PIN); // Set Open-Drain for chosen SCL1 and SDA1
 
     // c. Select HIGH SPEED for pins
-    GPIOB->OSPEEDR |= (0x3 << (8 * 2)) | (0x3 << (9 * 2)); // High speed for PB8 and PB9
+    GPIOB->OSPEEDR |= (0x3 << (SCL1_PIN * 2)) | (0x3 << (SDA1_PIN * 2)); // High speed for chosen SCL1 and SDA1
 
     // d. Select PULL UP for pins
-    GPIOB->PUPDR &= ~((0x3 << (8 * 2)) | (0x3 << (9 * 2))); // Clear PUPDR8 and PUPDR9
-    GPIOB->PUPDR |= (0x1 << (8 * 2)) | (0x1 << (9 * 2));    // Set Pull-Up for PB8 and PB9
+    GPIOB->PUPDR &= ~((0x3 << (SCL1_PIN * 2)) | (0x3 << (SDA1_PIN * 2))); // Clear PUPDR for SCL1 and SDA1
+    GPIOB->PUPDR |= (0x1 << (SCL1_PIN * 2)) | (0x1 << (SDA1_PIN * 2));    // Set Pull-Up for chosen SCL1 and SDA1
 
     // e. Configure Alternate Function in AFR register
-    GPIOB->AFR[1] &= ~((0xF << ((8 - 8) * 4)) | (0xF << ((9 - 8) * 4))); // Clear AFR for PB8 and PB9
-    GPIOB->AFR[1] |= (0x4 << ((8 - 8) * 4)) | (0x4 << ((9 - 8) * 4));    // Set AF4 (I2C1) for PB8 and PB9
+    if (SCL1_PIN >= 8) {
+        GPIOB->AFR[1] &= ~(0xF << ((SCL1_PIN - 8) * 4)); // Clear AFR for SCL1
+        GPIOB->AFR[1] |= (0x4 << ((SCL1_PIN - 8) * 4));  // Set AF4 (I2C1) for SCL1
+    } else {
+        GPIOB->AFR[0] &= ~(0xF << (SCL1_PIN * 4)); // Clear AFR for SCL1
+        GPIOB->AFR[0] |= (0x4 << (SCL1_PIN * 4));  // Set AF4 (I2C1) for SCL1
+    }
+    if (SDA1_PIN >= 8) {
+        GPIOB->AFR[1] &= ~(0xF << ((SDA1_PIN - 8) * 4)); // Clear AFR for SDA1
+        GPIOB->AFR[1] |= (0x4 << ((SDA1_PIN - 8) * 4));  // Set AF4 (I2C1) for SDA1
+    } else {
+        GPIOB->AFR[0] &= ~(0xF << (SDA1_PIN * 4)); // Clear AFR for SDA1
+        GPIOB->AFR[0] |= (0x4 << (SDA1_PIN * 4));  // Set AF4 (I2C1) for SDA1
+    }
 
     // 3. Reset the I2C
     I2C1->CR1 |= I2C_CR1_SWRST;
@@ -55,7 +55,6 @@ void i2c_config(void){
 
     // 7. Program the I2C_CR1 register to enable the peripheral
     I2C1->CR1 |= I2C_CR1_PE;
-	
 }
 
 void i2c_startSeq(void){
